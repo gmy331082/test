cea81ad2153ae07fdab16d3271f850ab0f013be0
gmy <gmy331082@163.com>
2017-04-02 22:29:47 +0800

    init

---
+++ src/main/java/org/gmy/Main.java
@@ -0,0 +1,20 @@
+package org.gmy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by chenghao on 2017/3/18.
+ */
+public class Main {
+    public static void main(String args[]){
+        WebDriver driver=new FirefoxDriver();
+        driver.get("http://www.baidu.com");
+        driver.findElement(By.xpath(".//*[@id='kw']")).sendKeys("aaaaa");
+        driver.findElement(By.xpath(".//*[@id='su']")).click();
+        //System.out.println(driver.getPageSource());
+        //driver.close();
+    }
+}
